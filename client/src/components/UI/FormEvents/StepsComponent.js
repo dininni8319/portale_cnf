0ee97ffd551052c02e1f504d270044abc62d00e8
Salvatore Dininni <s.dininni@yahoo.com>
2022-11-17 18:05:46 +0100

    implementatione di stile form multistep

--- client/src/components/UI/FormEvents/StepsComponent.js
+++ client/src/components/UI/FormEvents/StepsComponent.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 const StepsComponents = ({ 
   handlePrevStep,
   handleNextStep,
@@ -6,9 +9,15 @@ const StepsComponents = ({
 }) => {
   return ( 
      <div className='d-flex justify-content-between py-3 col-md-5 '>
-       <button className="text-green-dark fs-5 fw-bold" onClick={handlePrevStep}>Indietro</button>
+      { step !== 1 && <button className="text-green-dark fs-5 fw-bold" onClick={handlePrevStep}>
+          <FontAwesomeIcon icon={faAngleLeft} size="1x" className="pe-1 text-green-dark"/>
+          Indietro
+      </button>}
        { step === 5 && <button disabled={`${step === 5 ? '' : 'disabled'}`} className='btn btn-outline-success' onClick={handleEvent}>Salva Richiesta</button>}
-       <button type='button' className="btn-color-green text-white fw-bold" onClick={handleNextStep}>Avanti</button>
+      { step !== 5 && <button type='button' className="btn-color-green text-white fw-bold" onClick={handleNextStep}>
+          Avanti
+          <FontAwesomeIcon icon={faAngleRight} size="1x" className="ps-1 text-white"/> 
+       </button>}
      </div>
   );
 }
