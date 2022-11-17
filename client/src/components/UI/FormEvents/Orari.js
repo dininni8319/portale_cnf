0a1133dc7561c59339925f7ed9f8286c97fbb8f3
Salvatore Dininni <s.dininni@yahoo.com>
2022-11-17 18:22:37 +0100

    implementazioni frontend

--- client/src/components/UI/FormEvents/Orari.js
+++ client/src/components/UI/FormEvents/Orari.js
@@ -3,13 +3,13 @@ const Orari = ({ date, time }) => {
     <section>
         <h4>Orari</h4>
         <div className="mb-3">
-          <label for="inputDate" className="col-sm-2 col-form-label">Date</label>
+          <label htmlFor="inputDate" className="col-sm-2 col-form-label">Date</label>
           <div className="col-sm-10">
             <input type="date" className="form-control" id="inputDate" {...date}/>
           </div>
         </div>
         <div className="mb-3">
-          <label for="inputDate" className="col-sm-2 col-form-label">Time</label>
+          <label htmlFor="inputDate" className="col-sm-2 col-form-label">Time</label>
           <div className="col-sm-10">
             <input type="time" className="form-control" id="inputDate" {...time}/>
           </div>
--- client/src/components/UI/FormEvents/Riepilogo.js
+++ client/src/components/UI/FormEvents/Riepilogo.js
@@ -14,55 +14,46 @@ const Riepilogo = ({ config }) => {
 
   return ( 
     <section className='component-wrapper'>
-          <div className="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end completed">
-              <div className="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
-                <span className="d-block h-100 title-medium text-uppercase">Luogo: {ufficio}</span>
-              </div>
+        
+          <div className="info-progress-body d-flex justify-content-between align-items-end py-3 col-12 px-2">
+            <span className="title-medium text-uppercase fw-bold">Luogo:</span>
+            <span className="">{ufficio}</span>
           </div>
-           
-          <div className="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end completed">
-            <div className="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
-              <span className="d-block h-100 title-medium text-uppercase">Data e orario: {date} - {time}</span>
-            </div>
+        
+          <div className="info-progress-body d-flex justify-content-between align-items-end py-3 col-12 px-2">
+            <span className="title-medium text-uppercase fw-bold">Data e orario:</span>
+            <span className="">{date}  {time}</span>
           </div>
 
-            <div className="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end completed">
-              <div className="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
-                <span className="d-block h-100 title-medium text-uppercase">Dettagli appuntamento</span>
-                <ul>
-                  <li>Motivo: {tipologiaRichiesta}</li>
-                  <li>Dettaglio: {description}</li>
-                </ul>
-              </div>
-            </div>
-            
-            <div className="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end completed">
-              <div className="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
-                <span className="d-block h-100 title-medium text-uppercase">Richiedente</span>
-                <ul>
-                  <li>Email: {email}</li>
-                  <li>Nome: {first_name}</li>
-                  <li>Cognome: {last_name}</li>
-                  <li>Telefono: {phone}</li>
-                  <li>Cod/Fiscale: {codicefiscale}</li>
-                </ul>
-              </div>
-            </div>
-      
-            <div className="info-progress-wrapper d-none d-lg-flex w-100 px-3 flex-column justify-content-end step-active">
-              <div className="info-progress-body d-flex justify-content-between align-self-end align-items-end w-100 py-3">
-                <span className="d-block h-100 title-medium text-uppercase">Riepilogo</span>
-              </div>
-            </div>
+
+          <div className="info-progress-body d-flex flex-column py-3 col-12 px-2">
+            <span className="title-medium text-uppercase fw-bold">Dettagli appuntamento</span>
+            <ul>
+              <li><span className="title-medium text-capitalize fw-bold">Motivo: </span>{tipologiaRichiesta}</li>
+              <li><span className="title-medium text-capitalize fw-bold">Dettaglio: </span>{description}</li>
+            </ul>
+          </div>
+
+          <div className="info-progress-body d-flex flex-column py-3 col-12 px-2">
+            <span className="d-block h-100 title-medium text-uppercase">Richiedente</span>
+            <ul>
+              <li>Email: {email}</li>
+              <li>Nome: {first_name}</li>
+              <li>Cognome: {last_name}</li>
+              <li>Telefono: {phone}</li>
+              <li>Cod/Fiscale: {codicefiscale}</li>
+            </ul>
+             
+          </div>
           
-            <div className="iscrizioni-header d-lg-none w-100">
-              <h4 className="step-title d-flex align-items-center justify-content-between drop-shadow">
-                <span className="d-block d-lg-inline">
-                  Verifica dei dati
-                </span>
-                <span className="step">5/5</span>
-              </h4>
-            </div>
+          <div className="iscrizioni-header d-lg-none w-100">
+            <h4 className="step-title d-flex align-items-center justify-content-between drop-shadow">
+              <span className="d-block d-lg-inline">
+                Verifica dei dati
+              </span>
+              <span className="step">5/5</span>
+            </h4>
+          </div>
     
     </section>
    );
