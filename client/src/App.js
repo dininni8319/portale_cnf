a2d73f7b43ceb4823ddc2f36b652ceeba5a847d9
Salvatore Dininni <s.dininni@yahoo.com>
2022-11-21 11:06:55 +0100

    salvataggio modifiche fronend

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,11 @@ const App = () => {
       <div className="custom-height-class">
          <FormEvents />
       </div>
-      {/* //calendario funziona */}
-      {/* <iframe 
+      
+    <iframe 
       src="https://calendar.google.com/calendar/embed?src=67cb10fc4ca65505e0149eb3b93e9fe1279f95ff12403a915c13a1c656381376%40group.calendar.google.com&ctz=Europe%2FZurich" 
       style={{border: '0'}} 
-      width="800" height="600" frameBorder="0" scrolling="no"></iframe> */}
+      width="800" height="600" frameBorder="0" scrolling="no"></iframe>
       <Footer />
     </>
 
